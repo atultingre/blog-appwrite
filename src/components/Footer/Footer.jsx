a4408693d616c855681a22d13239a40b07d4ddc1
Atul Tingre <atultingre.work@gmail.com>
2024-03-06 01:02:55 +0530

    reusable postCard, Select & login form created.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../Logo";
+import Logo from "../reusable/Logo";
 
 function Footer() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,21 @@
-import Button from "./Button";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import LogoutBtn from "./Header/LogoutBtn";
-import Logo from "./Logo";
 import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Button from "./reusable/Button";
+import Input from "./reusable/Input";
+import Logo from "./reusable/Logo";
+import Select from "./reusable/Select";
+import PostCard from "./reusable/PostCard";
 
-export { Footer, Header, Logo, Container, LogoutBtn, Button };
+export {
+  Footer,
+  Header,
+  Logo,
+  Container,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  PostCard,
+};
