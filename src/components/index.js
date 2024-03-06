a8657bb2a72b7ebe11f8809ddff52ffad0f65152
Atul Tingre <atultingre.work@gmail.com>
2024-03-06 10:42:11 +0530

    Signup form & protected routes component created.

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,9 @@ import Input from "./reusable/Input";
 import Logo from "./reusable/Logo";
 import Select from "./reusable/Select";
 import PostCard from "./reusable/PostCard";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import Protected from "./auth/Protected";
 
 export {
   Footer,
@@ -18,4 +21,7 @@ export {
   Input,
   Select,
   PostCard,
+  Login,
+  Signup,
+  Protected,
 };
